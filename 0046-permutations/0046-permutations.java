a7d19259edf932ad2a68fbb5b0579a1e49b9b285
Arpit Kumar <arpitkumar09ps@gmail.com>
2025-07-04 10:09:28 +0530

    Time: 2 ms (37.87%), Space: 44.4 MB (91.7%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,18 @@
+class Solution {
+    private void solver(int[] nums, List<List<Integer>> res,List<Integer> temp,int start){
+        // according to testcase we have to write it in the size of three only.
+        if(temp.size() == nums.length) res.add(new ArrayList<>(temp));
+        for(int i = 0 ; i < nums.length;i++){
+            if(temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            solver(nums,res,temp,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        solver(nums,res,temp,0);
+        return res;
+    }
+}
