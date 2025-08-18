c31ef19dc91ccb9ad73f9ec8cea617ac378b86ac
Arpit Kumar <arpit.kumar231@lpu.in>
2025-08-18 23:14:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (67.9%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // Count of elements not equal to val
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // Move the non-val element to the front
+                k++;
+            }
+        }
+        return k;
+    }
+}
