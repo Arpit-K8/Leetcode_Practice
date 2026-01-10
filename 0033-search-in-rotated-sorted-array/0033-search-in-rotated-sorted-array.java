53dfa8a6c88f13fcf5f4a0bb790d49c06f1fe78f
Arpit Kumar <arpit.kumar231@lpu.in>
2026-01-11 02:26:27 +0530

    Time: 0 ms (100%), Space: 43.8 MB (48.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
-
         while (low <= high) {
             int mid = (low + high) / 2;
             if (nums[mid] == target) return mid;
@@ -14,7 +13,6 @@ class Solution {
                 else high = mid - 1;
             }
         }
-
-        return -1; // target not found
+        return -1;
     }
 }
