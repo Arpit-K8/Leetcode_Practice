ca3260bf30196bd0ae89e824ccb6529b955724ae
Arpit Kumar <arpit.kumar231@lpu.in>
2026-01-23 15:33:35 +0530

    Time: 3 ms (27.34%), Space: 65.2 MB (68.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] answer = new int[n];
-        // Step 1: Build prefix product
-        int prefix = 1;
-        for (int i = 0; i < n; i++) {
-            answer[i] = prefix;
-            prefix *= nums[i];
+    public int[] productExceptSelf(int[] nums) { // [1,2,3,4]
+        int[] left = new int[nums.length]; // [0 0 0 0]
+        int[] right = new int[nums.length];
+
+        left[0] = 1; // [1 0 0 0]
+        for (int i = 1; i < nums.length; i++) { 
+            left[i] = left[i - 1] * nums[i - 1]; // [1,1,2,6]
         }
-        // Step 2: Multiply with suffix product
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;
-            suffix *= nums[i];
+        right[nums.length - 1] = 1;// [0 0 0 1]
+        for (int i = nums.length - 2; i > -1; i--) {
+            right[i] = right[i + 1] * nums[i + 1]; // [24,12,4,1]
         }
-        return answer;
+
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = left[i] * right[i]; // [24,12,8,6]
+        }
+        return ans;
     }
 }
