767086a59558f5050a6d3f2b2d1d3413558c1e30
Arpit Kumar <arpit.kumar231@lpu.in>
2026-01-26 00:30:57 +0530

    Time: 1 ms (99.6%), Space: 77.5 MB (17.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -12,12 +12,10 @@ class Solution {
 
         int sum     = nums[0];
         int maxSum  = nums[0];
-        
         for(int i = 1; i<nums.length; i++) {
             sum     = Math.max(sum + nums[i], nums[i]);
             maxSum  = Math.max(maxSum, sum);
         }
-        
         return maxSum;
     }
 }
