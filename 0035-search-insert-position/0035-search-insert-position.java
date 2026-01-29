6a8ee46e672c4b3e9936bf413dc7326464399e08
Arpit Kumar <arpit.kumar231@lpu.in>
2026-01-29 10:28:42 +0530

    Time: 0 ms (100%), Space: 44.8 MB (59.45%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,41 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= target) {
-                return i;
-            }
+    public int searchInsert(int[] arr, int target) {
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] >= target) {
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
+        int left = 0, right = arr.length; // [left, right)
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) left = mid + 1;
+            else right = mid;
         }
-        return nums.length;
+        return left; // first index where arr[i] >= target
     }
 }
+
+
+
+
+
+// class Solution {
+//     int lowerBound(int[] arr, int target) {
+//         int left = 0, right = arr.length; // [left, right)
+
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (arr[mid] < target)
+//                 left = mid + 1;
+//             else
+//                 right = mid;
+//         }
+//         return left; // first index where arr[i] >= target
+//     }
+
+//     public int searchInsert(int[] nums, int target) {
+//         return lowerBound(nums, target);
+//     }
+// }
