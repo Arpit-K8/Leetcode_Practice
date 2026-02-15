e0452a698b677048559375f16508563a1cfd622a
Arpit Kumar <arpit.kumar231@lpu.in>
2026-02-15 23:15:26 +0530

    Time: 3 ms (73.97%), Space: 50.4 MB (37.71%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,18 +1,14 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
+        int m = grid.length,n = grid[0].length;
         // Initialize first row (can only come from left)
         for (int j = 1; j < n; j++) {
             grid[0][j] += grid[0][j - 1];
         }
-
         // Initialize first column (can only come from top)
         for (int i = 1; i < m; i++) {
             grid[i][0] += grid[i - 1][0];
         }
-
         // Fill the rest of the grid
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
@@ -22,7 +18,6 @@ class Solution {
                 );
             }
         }
-
         return grid[m - 1][n - 1]; // bottom-right cell holds result
     }
 }
