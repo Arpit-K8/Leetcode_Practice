fd98c2ed359e5db9c436b75af42a5d44433de0f6
Arpit Kumar <arpit.kumar231@lpu.in>
2026-02-16 14:52:17 +0530

    Time: 0 ms (100%), Space: 44.4 MB (52.44%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,20 +1,15 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String> list = new ArrayList<>();
-        int k=0;
-        int curr = target[k];
-        for(int i=1;i<=n;i++){
+        List<String> res = new ArrayList<>();
+        int index = 0;  // pointer for target
+        for(int i = 1; i <= n; i++){
+            if(index == target.length) break;
+            res.add("Push");
             
-            if(curr != i){
-                list.add("Push");
-                list.add("Pop");
-            }else{
-                list.add("Push");
-                k++;
-                if(k>=target.length) break;
-                curr = target[k];
-            }
+            if(i == target[index]) index++;  // move to next target element
+            else res.add("Pop");
         }
-        return list;
+        
+        return res;
     }
 }
