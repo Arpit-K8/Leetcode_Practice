cd0107fa84cada1b568fba5e44ed806d8165871f
Arpit Kumar <arpit.kumar231@lpu.in>
2026-02-18 23:11:46 +0530

    Time: 115 ms (27.93%), Space: 48.7 MB (5.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,15 +2,10 @@ class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         if (n == 0) return "";
-
         boolean[][] dp = new boolean[n][n];
         int start = 0, maxLen = 1;
-
         // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-        }
-
+        for (int i = 0; i < n; i++) dp[i][i] = true;
         // Check substrings of length 2
         for (int i = 0; i < n - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
@@ -19,7 +14,6 @@ class Solution {
                 maxLen = 2;
             }
         }
-
         // Check substrings of length > 2
         for (int len = 3; len <= n; len++) {
             for (int i = 0; i <= n - len; i++) {
@@ -31,7 +25,6 @@ class Solution {
                 }
             }
         }
-
         return s.substring(start, start + maxLen);
     }
 }
