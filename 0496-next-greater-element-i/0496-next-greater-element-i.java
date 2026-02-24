cedd0f1c9556d30e0bec80d5abe2e853ebd0e6a4
Arpit Kumar <arpit.kumar231@lpu.in>
2026-02-25 00:11:31 +0530

    Time: 4 ms (73.48%), Space: 45.5 MB (33.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums2.length;
-        int[] nextGreaterElements = new int[n];
-        Stack<Integer> helperStack = new Stack<>();
-        // Step 1: Compute NGE for nums2
-        for (int i = n - 1; i >= 0; i--) {
-            int element = nums2[i];
-            while (!helperStack.isEmpty() && helperStack.peek() <= element) {
-                helperStack.pop();
-            }
-            if (helperStack.isEmpty()) nextGreaterElements[i] = -1;
-            else nextGreaterElements[i] = helperStack.peek();
-            helperStack.push(element);
-        }
-        // Step 2: Store value -> NGE mapping
+        int[] res = new int[nums1.length];
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums2[i], nextGreaterElements[i]);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i <n; i++) {
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                map.put(st.pop(),nums2[i]);
+            }
+            st.push(nums2[i]);
         }
-        // Step 3: Build result for nums1
-        int[] result = new int[nums1.length];
+        for (int num : st){ // for rest element in stack
+            map.put(num,-1);
+        }
         for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]);
+            res[i] = map.get(nums1[i]); // now find ele of arr1 in arr2 from there we will find NGE
         }
-        return result;
+        return res;
     }
 }
