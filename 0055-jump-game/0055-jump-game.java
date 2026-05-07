b81c12c3eb988ba5065c9fdf06dca7ab6093d03e
Arpit Kumar <arpit.kumar231@lpu.in>
2026-05-08 00:01:00 +0530

    Time: 2 ms (87.16%), Space: 47.8 MB (61.35%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,23 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int reached = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= reached) {
-                reached = i; // Shift the target to the curernt index
-            }
-        }
-        return reached == 0;
+    // public boolean canJump(int[] nums) {
+    //     int n = nums.length;
+    //     int reached = n - 1;
+    //     for (int i = n - 2; i >= 0; i--) {
+    //         if (i + nums[i] >= reached) {
+    //             reached = i; // Shift the target to the curernt index
+    //         }
+    //     }
+    //     return reached == 0;
+    // }
+    //Sir's method
+    
+    public boolean canJump(int[] arr) {
+    int range = 0;
+    for (int i = 0; i < arr.length; i++) {
+        if (i > range) return false;
+        range = Math.max(range, arr[i] + i);
+        if (range >= arr.length - 1) return true;
+    }
+    return true;
     }
 }
