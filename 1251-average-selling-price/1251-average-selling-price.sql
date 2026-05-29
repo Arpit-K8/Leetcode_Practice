4e8d559719025a32d767525d6ce2610e3af622d7
Arpit Kumar <arpit.kumar231@lpu.in>
2026-05-29 19:58:28 +0530

    Time: 1155 ms (5.48%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id;
+
+-- testcase issue so i have to make sure if null or not exist then i have to return 0 so i used IFNULL(....,0)
